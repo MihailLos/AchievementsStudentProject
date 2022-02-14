e592e5af3dadaacb4884cf072e180aa3e566ade9
ender <enderclub12@gmail.com>
2022-02-14 18:22:21 +0700

    Доработка дизайна экрана авторизации

--- app/src/main/java/com/example/achievementsstudent/activities/LoginActivity.kt
+++ app/src/main/java/com/example/achievementsstudent/activities/LoginActivity.kt
@@ -7,15 +7,26 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -26,7 +37,7 @@ import com.example.achievementsstudent.R
 
 @Composable
 fun LoginActivity() {
-    Scaffold() {
+    Scaffold(backgroundColor = Color.White) {
         Column(
             Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -48,55 +59,89 @@ fun LoginActivity() {
                     fontStyle = FontStyle.Normal,
                     fontSize = 24.sp,
                     letterSpacing = 0.07.em,
-                    fontWeight = FontWeight.Bold
-                )
+                    fontWeight = FontWeight.Bold,
+
+                    )
                 Spacer(modifier = Modifier.height(21.dp))
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
+                    var username by remember {
+                        mutableStateOf("")
+                    }
+                    var password by remember {
+                        mutableStateOf("")
+                    }
+                    var isPasswordVisible by remember {
+                        mutableStateOf(false)
+                    }
                     TextField(
-                        value = "",
-                        onValueChange = {},
+                        value = username,
+                        onValueChange = { username = it },
                         label = { Text(text = "Логин Eios") },
                         modifier = Modifier
                             .width(366.dp)
-                            .height(46.dp)
-                            .background(Color(0xfff3f2f2))
-                            .border(BorderStroke(1.dp, Color(0xFFC4C4C4))),
-                        shape = RoundedCornerShape(10.dp)
+                            .height(60.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        singleLine = true,
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color(
+                                0xfff3f2f2
+                            )
+                        ),
+                        trailingIcon = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Clear,
+                                    contentDescription = "Clear icon"
+                                )
+                            }
+                        }
                     )
                     Spacer(modifier = Modifier.height(21.dp))
                     TextField(
-                        value = "",
-                        onValueChange = {},
+                        value = password,
+                        onValueChange = { password = it },
                         label = { Text(text = "Пароль") },
                         modifier = Modifier
                             .width(366.dp)
-                            .height(46.dp)
-                            .background(Color(0xfff3f2f2))
-                            .border(BorderStroke(1.dp, Color(0xFFC4C4C4))),
-                        shape = RoundedCornerShape(10.dp)
+                            .height(60.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        singleLine = true,
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color(
+                                0xfff3f2f2
+                            )
+                        ),
+                        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        trailingIcon = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                    contentDescription = "Visibilities icon"
+                                )
+                            }
+                        }
                     )
                     Spacer(modifier = Modifier.height(21.dp))
                     Button(
                         onClick = { /*TODO*/ },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFFF9966)),
                         modifier = Modifier
-                            .background(
-                                brush = Brush.linearGradient(
-                                    colors = listOf(
-                                        Color(0xFFFFBC89),
-                                        Color(0xFFFF9A67)
-                                    )
-                                )
-                            )
                             .width(366.dp)
-                            .height(46.dp)
-                            .shadow(6.dp),
+                            .height(46.dp),
                         shape = RoundedCornerShape(10.dp)
                     ) {
                         Text(text = "Войти", color = Color.White)
+                    }
+                    TextButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .padding(end = 20.dp)
+                    ) {
+                        Text(text = "Не получается войти?", color = Color(0xFFFF9966))
                     }
                 }
             }
@@ -107,5 +152,7 @@ fun LoginActivity() {
 @Preview
 @Composable
 fun LoginActivityPreview() {
-    LoginActivity()
+    MaterialTheme() {
+        LoginActivity()
+    }
 }
